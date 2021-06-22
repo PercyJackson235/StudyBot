de477bd15f11eeee611ac76db15f6f954c64f94a
Josh Levin <floydaroo@gmail.com>
2021-06-22 01:29:29 -0400

    fixed db integration in the github-join command

--- github_cog.py
+++ github_cog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from github import Github
-import closing
+from contextlib import closing
 import studybot
 
 
@@ -19,28 +19,39 @@ class GitHub_Integration(commands.Cog):
             return
         else:
             g = Github(studybot.TOKENS.get('GITHUB_API_KEY'))
-            # Get the Organization object and the user object.
-            g_user = g.get_user(github_name)
-            g_org = g.get_organization(studybot.TOKENS.get('GITHUB_ORG_NAME'))
+            # Get the user object.
+            try:
+                g_user = g.get_user(github_name)
+            except:
+                await ctx.message.reply('The GitHub user you requested cannot be found. Double-Check your spelling.')
+                return()
+            # Get the organization object.
+            try:
+                g_org = g.get_organization(studybot.TOKENS.get('GITHUB_ORG_NAME'))
+            except:
+                msg = 'The GitHub organization cannot be found. Please try again later, '
+                msg += 'or contact a mod if the issue persists.'
+                await ctx.message.reply(msg)
+                return()
 
             # Verify that the user has not already invited a GitHub account to the org.
             with closing(studybot.db_conn.cursor()) as conn:
                 async with studybot.lock:
-                    query = 'SELECT github_id FROM github_invites WHERE discord_id = ?'  # noqa: E501
-                    conn.execute(query, ctx.author.id)
+                    query = 'SELECT github_id FROM github_invites WHERE discord_id = ?'
+                    conn.execute(query, (ctx.author.id, ))
                     value = conn.fetchone()
                     if value is None:
                         # Invite the user to the organization.
                         g_org.invite_user(g_user)
                         # Add the Discord ID and the GitHub ID to the database table
-                        query = 'INSERT INTO study_time (discord_id, github_id) VALUES (?, ?)'
+                        query = 'INSERT INTO github_invites (discord_id, github_id) VALUES (?, ?)'
                         conn.execute(query, (ctx.author.id, g_user.id))
                         # Send a confirmation message in Discord.
-                        ctx.message.reply(f'Invited {g_user.name} to the GitHub Organization.')
+                        await ctx.message.reply(f'Invited {g_user.name} to the GitHub Organization.')
                     else:
-                        msg = f'Cannot invite {g_user.name} to the organization, because you have already invited '
+                        msg = f'Cannot invite {g_user.login} to the organization, because you have already invited '
                         msg += 'another GitHub account. Please contact a moderator if this is a mistake.'
-                        ctx.message.reply(msg)
+                        await ctx.message.reply(msg)
                     studybot.db_conn.commit()
 
 
